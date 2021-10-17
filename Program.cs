6b0bc42fac5ea1c4add9038820775da522b8c2ac
ts128 <t.sinczak@gmail.com>
2021-10-17 17:43:40 +0200

    drobna poprawka

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace shape
         static void Main(string[] args)
         {
             var rhombus = new FrameRhombus(7, 1);
-            var square = new FrameSquare(11, 4);            
+            var square = new FrameSquare(11, 4);
 
+            //rhombus.RenderManyFramesInRow(5);
             for (int i = 0; i < 10; i++)
-            {
-                //rhombus.RenderManyFramesInRow(5);
+            {                
                 rhombus.RenderFrames();
                 Thread.Sleep(100);
                 Console.Clear();
